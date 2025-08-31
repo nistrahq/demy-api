dbed47f23a91d2a5eecaf4381abbd847d53db9de
Salim Ramirez <salimramirezm@gmail.com>
2025-08-31 11:31:25 -0500

    feat(institution): add AdministratorResourceFromEntityAssembler for transforming Administrator entities to resources

---
+++ src/main/java/com/nistra/demy/platform/institution/interfaces/rest/transform/AdministratorResourceFromEntityAssembler.java
@@ -0,0 +1,17 @@
+package com.nistra.demy.platform.institution.interfaces.rest.transform;
+
+import com.nistra.demy.platform.institution.domain.model.aggregates.Administrator;
+import com.nistra.demy.platform.institution.interfaces.rest.resources.AdministratorResource;
+
+public class AdministratorResourceFromEntityAssembler {
+    public static AdministratorResource toResourceFromEntity(Administrator entity) {
+        return new AdministratorResource(
+                entity.getId(),
+                entity.getPersonName().firstName(),
+                entity.getPersonName().lastName(),
+                entity.getPhoneNumber().getFullNumber(),
+                entity.getDniNumber().dniNumber(),
+                entity.getAcademyId().academyId()
+        );
+    }
+}
