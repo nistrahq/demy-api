09eed091f9b1a997c297e2c957bbc028557344f7
Salim Ramirez <salimramirezm@gmail.com>
2025-08-31 00:18:13 -0500

    feat(email): add UserNotificationEmailService interface for user notification email handling

---
+++ src/main/java/com/nistra/demy/platform/iam/infrastructure/email/javamailsender/UserNotificationEmailService.java
@@ -0,0 +1,6 @@
+package com.nistra.demy.platform.iam.infrastructure.email.javamailsender;
+
+import com.nistra.demy.platform.iam.application.internal.outboundservices.email.EmailService;
+
+public interface UserNotificationEmailService extends EmailService {
+}
