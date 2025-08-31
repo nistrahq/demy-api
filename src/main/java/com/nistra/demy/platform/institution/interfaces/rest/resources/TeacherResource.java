b8b559c13936329225f5454385a85f248c8b762f
Salim Ramirez <salimramirezm@gmail.com>
2025-08-31 11:29:36 -0500

    feat(institution): add TeacherResource for transferring teacher data

---
+++ src/main/java/com/nistra/demy/platform/institution/interfaces/rest/resources/TeacherResource.java
@@ -0,0 +1,8 @@
+package com.nistra.demy.platform.institution.interfaces.rest.resources;
+
+public record TeacherResource(
+        String firstName,
+        String lastName,
+        Long academyId
+) {
+}
