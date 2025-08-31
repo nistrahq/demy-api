a4b5192f0b8889392664de51db509efcfb3f3b0f
Salim Ramirez <salimramirezm@gmail.com>
2025-08-31 00:19:08 -0500

    feat(iam): add SpringSecurityCurrentUserProvider interface extending IdentityService

---
+++ src/main/java/com/nistra/demy/platform/iam/infrastructure/identity/springsecurity/SpringSecurityCurrentUserProvider.java
@@ -0,0 +1,6 @@
+package com.nistra.demy.platform.iam.infrastructure.identity.springsecurity;
+
+import com.nistra.demy.platform.iam.application.internal.outboundservices.identity.IdentityService;
+
+public interface SpringSecurityCurrentUserProvider extends IdentityService {
+}
