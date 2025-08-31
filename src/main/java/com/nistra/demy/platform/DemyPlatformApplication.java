f333a73a9589e6ff4f03597a8a45637e741b9969
Salim Ramirez <salimramirezm@gmail.com>
2025-08-30 23:12:30 -0500

    chore: initial commit

---
+++ src/main/java/com/nistra/demy/platform/DemyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.nistra.demy.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemyPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemyPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nistra/demy/platform/DemyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nistra.demy.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemyPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
