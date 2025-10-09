5fbae73c81c823331dc6921a82f4a13c0d2c4e76
Kyrubi <u20221c486@upc.edu.pe>
2025-10-09 12:36:10 -0500

    feat(enrollment): add CreateAcademicPeriodCommandFromResourceAssembler for resource conversion

---
+++ src/main/java/com/nistra/demy/platform/enrollment/interfaces/rest/resources/CreateAcademicPeriodResource.java
@@ -0,0 +1,31 @@
+package com.nistra.demy.platform.enrollment.interfaces.rest.resources;
+
+import com.nistra.demy.platform.shared.domain.model.valueobjects.AcademyId;
+
+import java.time.LocalDate;
+
+/**
+ * Create academic period resource.
+ */
+public record CreateAcademicPeriodResource(String periodName, LocalDate startDate, LocalDate endDate, Boolean isActive) {
+
+    /**
+     * Validates the resource.
+     * @throws IllegalArgumentException if any field is null or if the dates are invalid.
+     */
+    public CreateAcademicPeriodResource {
+        if (periodName == null || periodName.isBlank()) {
+            throw new IllegalArgumentException("Name is required");
+        }
+        if (startDate == null) {
+            throw new IllegalArgumentException("Start date is required");
+        }
+        if (endDate == null) {
+            throw new IllegalArgumentException("End date is required");
+        }
+        if (endDate.isBefore(startDate)) {
+            throw new IllegalArgumentException("End date cannot be before start date");
+        }
+
+    }
+}
