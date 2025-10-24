573fad278a87edde8c4f4c53b3588f433681b252
Salim Ramirez <salimramirezm@gmail.com>
2025-10-24 12:43:59 -0500

    feat(institution): add teacher registration functionality in acl

--- src/main/java/com/nistra/demy/platform/iam/interfaces/acl/IamContextFacade.java
+++ src/main/java/com/nistra/demy/platform/iam/interfaces/acl/IamContextFacade.java
@@ -1,5 +1,9 @@
 package com.nistra.demy.platform.iam.interfaces.acl;
 
+import java.util.List;
+
 public interface IamContextFacade {
+    Long signUpVerifiedUser(String email, List<String> roles);
+
     Long fetchAuthenticatedUserTenantId();
 }
--- src/main/java/com/nistra/demy/platform/institution/application/internal/outboundservices/acl/ExternalIamService.java
+++ src/main/java/com/nistra/demy/platform/institution/application/internal/outboundservices/acl/ExternalIamService.java
@@ -1,9 +1,12 @@
 package com.nistra.demy.platform.institution.application.internal.outboundservices.acl;
 
 import com.nistra.demy.platform.iam.interfaces.acl.IamContextFacade;
+import com.nistra.demy.platform.institution.domain.model.valueobjects.InstitutionRoles;
+import com.nistra.demy.platform.institution.domain.model.valueobjects.UserId;
 import com.nistra.demy.platform.shared.domain.model.valueobjects.AcademyId;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +21,10 @@ public class ExternalIamService {
     public Optional<AcademyId> fetchCurrentAcademyId() {
         var academyId = iamContextFacade.fetchAuthenticatedUserTenantId();
         return academyId == 0L ? Optional.empty() : Optional.of(new AcademyId(academyId));
+    }
+
+    public Optional<UserId> registerTeacher(String email) {
+        var userId = iamContextFacade.signUpVerifiedUser(email, List.of(InstitutionRoles.ROLE_TEACHER.name()));
+        return userId == 0L ? Optional.empty() : Optional.of(new UserId(userId));
     }
 }
