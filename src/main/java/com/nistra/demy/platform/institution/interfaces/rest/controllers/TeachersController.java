b620be8d6d45617989c488e77ec9e98a4b82ee9c
Salim Ramirez <salimramirezm@gmail.com>
2025-10-24 12:46:29 -0500

    feat(institution): add teacher registration endpoint in TeachersController

--- src/main/java/com/nistra/demy/platform/institution/interfaces/rest/controllers/TeachersController.java
+++ src/main/java/com/nistra/demy/platform/institution/interfaces/rest/controllers/TeachersController.java
@@ -1,14 +1,16 @@
 package com.nistra.demy.platform.institution.interfaces.rest.controllers;
 
 import com.nistra.demy.platform.institution.domain.model.queries.GetAllTeachersQuery;
+import com.nistra.demy.platform.institution.domain.services.TeacherCommandService;
 import com.nistra.demy.platform.institution.domain.services.TeacherQueryService;
+import com.nistra.demy.platform.institution.interfaces.rest.resources.RegisterTeacherResource;
 import com.nistra.demy.platform.institution.interfaces.rest.resources.TeacherResource;
+import com.nistra.demy.platform.institution.interfaces.rest.transform.RegisterTeacherCommandFromResourceAssembler;
 import com.nistra.demy.platform.institution.interfaces.rest.transform.TeacherResourceFromEntityAssembler;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,10 +21,22 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Tag(name = "Teachers", description = "Endpoints for managing teachers")
 public class TeachersController {
 
+    private final TeacherCommandService teacherCommandService;
     private final TeacherQueryService teacherQueryService;
 
-    public TeachersController(TeacherQueryService teacherQueryService) {
+    public TeachersController(TeacherCommandService teacherCommandService, TeacherQueryService teacherQueryService) {
+        this.teacherCommandService = teacherCommandService;
         this.teacherQueryService = teacherQueryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TeacherResource> registerTeacher(@RequestBody RegisterTeacherResource resource) {
+        var registerTeacherCommand = RegisterTeacherCommandFromResourceAssembler.toCommandFromResource(resource);
+        var teacher = teacherCommandService.handle(registerTeacherCommand);
+        if (teacher.isEmpty()) return ResponseEntity.badRequest().build();
+        var teacherEntity = teacher.get();
+        var teacherResource = TeacherResourceFromEntityAssembler.toResourceFromEntity(teacherEntity);
+        return new ResponseEntity<>(teacherResource, HttpStatus.CREATED);
     }
 
     @GetMapping
