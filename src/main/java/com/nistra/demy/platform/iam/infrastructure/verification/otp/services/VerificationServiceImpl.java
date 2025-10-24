c132ef2d5c8136f7d3eb79e7b01c43696bea0630
Salim Ramirez <salimramirezm@gmail.com>
2025-10-24 12:47:22 -0500

    chore: add TODO for rename in VerificationServiceImpl

--- src/main/java/com/nistra/demy/platform/iam/infrastructure/verification/otp/services/VerificationServiceImpl.java
+++ src/main/java/com/nistra/demy/platform/iam/infrastructure/verification/otp/services/VerificationServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
 
+// TODO: Rename to SecureCodeService
 @Service
 public class VerificationServiceImpl implements OtpSecureVerificationService {
 
