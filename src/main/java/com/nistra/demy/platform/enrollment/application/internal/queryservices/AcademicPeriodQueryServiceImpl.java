2e3d086366eef06e95804fe48880dc0b4c6a5b95
Kyrubi <u20221c486@upc.edu.pe>
2025-10-23 19:32:02 -0500

    feat(enrollment): add ExternalIamService to fetch current academyId in AcademicPeriodQueryServiceImpl

--- src/main/java/com/nistra/demy/platform/enrollment/application/internal/queryservices/AcademicPeriodQueryServiceImpl.java
+++ src/main/java/com/nistra/demy/platform/enrollment/application/internal/queryservices/AcademicPeriodQueryServiceImpl.java
@@ -1,10 +1,12 @@
 package com.nistra.demy.platform.enrollment.application.internal.queryservices;
 
+import com.nistra.demy.platform.enrollment.application.internal.outboundservices.acl.ExternalIamService;
 import com.nistra.demy.platform.enrollment.domain.model.aggregates.AcademicPeriod;
 import com.nistra.demy.platform.enrollment.domain.model.queries.GetAcademicPeriodByIdQuery;
 import com.nistra.demy.platform.enrollment.domain.model.queries.GetAllAcademicPeriodsQuery;
 import com.nistra.demy.platform.enrollment.domain.services.AcademicPeriodQueryService;
 import com.nistra.demy.platform.enrollment.infrastructure.persistence.jpa.repositories.AcademicPeriodRepository;
+import com.nistra.demy.platform.shared.domain.model.valueobjects.AcademyId;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,14 +15,20 @@ import java.util.Optional;
 @Service
 public class AcademicPeriodQueryServiceImpl implements AcademicPeriodQueryService {
     private final AcademicPeriodRepository academicPeriodRepository;
+    private final ExternalIamService externalIamService;
 
-    public AcademicPeriodQueryServiceImpl(AcademicPeriodRepository academicPeriodRepository) {
+    public AcademicPeriodQueryServiceImpl(
+            AcademicPeriodRepository academicPeriodRepository,
+            ExternalIamService externalIamService) {
         this.academicPeriodRepository = academicPeriodRepository;
+        this.externalIamService = externalIamService;
     }
 
     @Override
     public List<AcademicPeriod> handle(GetAllAcademicPeriodsQuery query) {
-        return academicPeriodRepository.findAll();
+        var academyId = externalIamService.fetchCurrentAcademyId()
+                .orElseThrow(() -> new IllegalStateException("Current academy ID not found"));
+        return academicPeriodRepository.findAllByAcademyId(academyId);
     }
 
     @Override
