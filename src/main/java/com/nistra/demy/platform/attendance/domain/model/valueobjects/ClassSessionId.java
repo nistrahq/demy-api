a460e27d1e7e4b5d86cd5561e107e7062c9393ec
diegovilcasaboya <diegovilcatut@gmail.com>
2025-10-26 21:03:13 -0500

    feat(attendance): add ClassSessionId value object

---
+++ src/main/java/com/nistra/demy/platform/attendance/domain/model/valueobjects/ClassSessionId.java
@@ -0,0 +1,31 @@
+package com.nistra.demy.platform.attendance.domain.model.valueobjects;
+
+import jakarta.persistence.Embeddable;
+
+/**
+ * Value object that represents the Id of a ClassSession
+ * @param classSessionId
+ */
+@Embeddable
+public record ClassSessionId(
+        Long classSessionId
+) {
+    /**
+     * Default constructor required by JPA.
+     * Initializes the classSessionID with a default value of 0.
+     */
+    public ClassSessionId() {
+        this(null);
+    }
+
+    /**
+     * Constructs an ClassSessionId with the specified ID.
+     *
+     * @param classSessionId the unique identifier for the academy
+     * @throws IllegalArgumentException if academyId is less than or equal to zero
+     */
+    public ClassSessionId {
+        if (classSessionId != null && classSessionId <= 0)
+            throw new IllegalArgumentException("ClassSession ID must be greater than zero");
+    }
+}
