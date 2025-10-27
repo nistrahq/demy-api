fd01cd2bc24d7f392bbbca3c0dba8addf910a5ee
Salim Ramirez <salimramirezm@gmail.com>
2025-10-27 16:11:15 -0500

    feat(billing): add AccountStatus enum and update BillingAccount with invoices and status

--- src/main/java/com/nistra/demy/platform/billing/domain/model/aggregates/BillingAccount.java
+++ src/main/java/com/nistra/demy/platform/billing/domain/model/aggregates/BillingAccount.java
@@ -1,10 +1,17 @@
 package com.nistra.demy.platform.billing.domain.model.aggregates;
 
+import com.nistra.demy.platform.billing.domain.model.entities.Invoice;
+import com.nistra.demy.platform.billing.domain.model.valueobjects.AccountStatus;
 import com.nistra.demy.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import com.nistra.demy.platform.shared.domain.model.valueobjects.AcademyId;
 import com.nistra.demy.platform.shared.domain.model.valueobjects.StudentId;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
+
+import java.util.Set;
 
 @Entity
 public class BillingAccount extends AuditableAbstractAggregateRoot<BillingAccount> {
@@ -12,4 +19,18 @@ public class BillingAccount extends AuditableAbstractAggregateRoot<BillingAccoun
     @Embedded
     @Getter
     private StudentId studentId;
+
+    @Getter
+    private Set<Invoice> invoices;
+
+    @Enumerated(EnumType.STRING)
+    private AccountStatus status;
+
+    @Embedded
+    @Getter
+    private AcademyId academyId;
+
+    protected BillingAccount() {}
+
+
 }
