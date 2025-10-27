de00283f9b05b0ebe3ed950c3489786099ed0408
Salim Ramirez <salimramirezm@gmail.com>
2025-10-27 16:17:01 -0500

    feat(billing): implement CreateBillingAccountCommand and update BillingAccount constructor

--- src/main/java/com/nistra/demy/platform/billing/domain/model/aggregates/BillingAccount.java
+++ src/main/java/com/nistra/demy/platform/billing/domain/model/aggregates/BillingAccount.java
@@ -1,5 +1,6 @@
 package com.nistra.demy.platform.billing.domain.model.aggregates;
 
+import com.nistra.demy.platform.billing.domain.model.commands.CreateBillingAccountCommand;
 import com.nistra.demy.platform.billing.domain.model.entities.Invoice;
 import com.nistra.demy.platform.billing.domain.model.valueobjects.AccountStatus;
 import com.nistra.demy.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
@@ -11,6 +12,8 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.Getter;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -20,7 +23,6 @@ public class BillingAccount extends AuditableAbstractAggregateRoot<BillingAccoun
     @Getter
     private StudentId studentId;
 
-    @Getter
     private Set<Invoice> invoices;
 
     @Enumerated(EnumType.STRING)
@@ -32,5 +34,14 @@ public class BillingAccount extends AuditableAbstractAggregateRoot<BillingAccoun
 
     protected BillingAccount() {}
 
+    public BillingAccount(CreateBillingAccountCommand command, AcademyId academyId) {
+        this.studentId = command.studentId();
+        this.invoices = new HashSet<>();
+        this.status = AccountStatus.ACTIVE;
+        this.academyId = academyId;
+    }
 
+    public Set<Invoice> getInvoices() {
+        return Collections.unmodifiableSet(invoices);
+    }
 }
