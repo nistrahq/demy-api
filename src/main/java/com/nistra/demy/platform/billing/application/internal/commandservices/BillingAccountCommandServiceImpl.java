7845984f01d313b68aa46119258e98fba4a04f8d
Salim Ramirez <salimramirezm@gmail.com>
2025-10-27 16:22:27 -0500

    feat(billing): implement BillingAccountCommandService for handling account creation

---
+++ src/main/java/com/nistra/demy/platform/billing/application/internal/commandservices/BillingAccountCommandServiceImpl.java
@@ -0,0 +1,38 @@
+package com.nistra.demy.platform.billing.application.internal.commandservices;
+
+import com.nistra.demy.platform.billing.application.internal.outboundservices.acl.ExternalIamService;
+import com.nistra.demy.platform.billing.domain.model.aggregates.BillingAccount;
+import com.nistra.demy.platform.billing.domain.model.commands.CreateBillingAccountCommand;
+import com.nistra.demy.platform.billing.domain.services.BillingAccountCommandService;
+import com.nistra.demy.platform.billing.infrastructure.persistence.jpa.repositories.BillingAccountRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BillingAccountCommandServiceImpl implements BillingAccountCommandService {
+
+    private final BillingAccountRepository billingAccountRepository;
+    private final ExternalIamService externalIamService;
+
+    public BillingAccountCommandServiceImpl(
+            BillingAccountRepository billingAccountRepository,
+            ExternalIamService externalIamService
+    ) {
+        this.billingAccountRepository = billingAccountRepository;
+        this.externalIamService = externalIamService;
+    }
+
+    @Override
+    public Optional<BillingAccount> handle(CreateBillingAccountCommand command) {
+        var academyId = externalIamService.fetchCurrentAcademyId()
+                .orElseThrow(() -> new RuntimeException("No academy found"));
+        var billingAccount = new BillingAccount(command, academyId);
+        try {
+            billingAccountRepository.save(billingAccount);
+            return Optional.of(billingAccount);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create billing account: %s".formatted(e.getMessage()));
+        }
+    }
+}
---
+++ src/main/java/com/nistra/demy/platform/billing/domain/services/BillingAccountCommandService.java
@@ -0,0 +1,10 @@
+package com.nistra.demy.platform.billing.domain.services;
+
+import com.nistra.demy.platform.billing.domain.model.aggregates.BillingAccount;
+import com.nistra.demy.platform.billing.domain.model.commands.CreateBillingAccountCommand;
+
+import java.util.Optional;
+
+public interface BillingAccountCommandService {
+    Optional<BillingAccount> handle(CreateBillingAccountCommand command);
+}
