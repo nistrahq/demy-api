936ed5724003b694e01849903c1433d797c748b1
Salim Ramirez <salimramirezm@gmail.com>
2025-10-28 09:53:15 -0500

    feat(billing): add query for retrieving billing account by ID

--- src/main/java/com/nistra/demy/platform/billing/application/internal/queryservices/BillingAccountQueryServiceImpl.java
+++ src/main/java/com/nistra/demy/platform/billing/application/internal/queryservices/BillingAccountQueryServiceImpl.java
@@ -1,14 +1,17 @@
 package com.nistra.demy.platform.billing.application.internal.queryservices;
 
+import com.nistra.demy.platform.billing.domain.model.aggregates.BillingAccount;
 import com.nistra.demy.platform.billing.domain.model.entities.Invoice;
 import com.nistra.demy.platform.billing.domain.model.queries.GetAllInvoicesByBillingAccountIdQuery;
 import com.nistra.demy.platform.billing.domain.model.queries.GetAllInvoicesByStudentIdQuery;
+import com.nistra.demy.platform.billing.domain.model.queries.GetBillingAccountByIdQuery;
 import com.nistra.demy.platform.billing.domain.services.BillingAccountQueryService;
 import com.nistra.demy.platform.billing.infrastructure.persistence.jpa.repositories.BillingAccountRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BillingAccountQueryServiceImpl implements BillingAccountQueryService {
@@ -17,6 +20,11 @@ public class BillingAccountQueryServiceImpl implements BillingAccountQueryServic
 
     public BillingAccountQueryServiceImpl(BillingAccountRepository billingAccountRepository) {
         this.billingAccountRepository = billingAccountRepository;
+    }
+
+    @Override
+    public Optional<BillingAccount> handle(GetBillingAccountByIdQuery query) {
+        return billingAccountRepository.findById(query.billingAccountId());
     }
 
     @Override
--- src/main/java/com/nistra/demy/platform/billing/domain/services/BillingAccountQueryService.java
+++ src/main/java/com/nistra/demy/platform/billing/domain/services/BillingAccountQueryService.java
@@ -1,12 +1,17 @@
 package com.nistra.demy.platform.billing.domain.services;
 
+import com.nistra.demy.platform.billing.domain.model.aggregates.BillingAccount;
 import com.nistra.demy.platform.billing.domain.model.entities.Invoice;
 import com.nistra.demy.platform.billing.domain.model.queries.GetAllInvoicesByBillingAccountIdQuery;
 import com.nistra.demy.platform.billing.domain.model.queries.GetAllInvoicesByStudentIdQuery;
+import com.nistra.demy.platform.billing.domain.model.queries.GetBillingAccountByIdQuery;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BillingAccountQueryService {
+    Optional<BillingAccount> handle(GetBillingAccountByIdQuery query);
+
     List<Invoice> handle(GetAllInvoicesByBillingAccountIdQuery query);
 
     List<Invoice> handle(GetAllInvoicesByStudentIdQuery query);
