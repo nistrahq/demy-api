ec076b1366584e2b04a86eee702206b715cc6373
Salim Ramirez <salimramirezm@gmail.com>
2025-10-27 19:15:12 -0500

    feat(billing): add assignInvoice method to BillingAccountsController for invoice assignment

--- src/main/java/com/nistra/demy/platform/billing/interfaces/rest/controllers/BillingAccountsController.java
+++ src/main/java/com/nistra/demy/platform/billing/interfaces/rest/controllers/BillingAccountsController.java
@@ -1,17 +1,16 @@
 package com.nistra.demy.platform.billing.interfaces.rest.controllers;
 
 import com.nistra.demy.platform.billing.domain.services.BillingAccountCommandService;
+import com.nistra.demy.platform.billing.interfaces.rest.resources.AssignInvoiceResource;
 import com.nistra.demy.platform.billing.interfaces.rest.resources.BillingAccountResource;
 import com.nistra.demy.platform.billing.interfaces.rest.resources.CreateBillingAccountResource;
+import com.nistra.demy.platform.billing.interfaces.rest.transform.AssignInvoiceCommandFromResourceAssembler;
 import com.nistra.demy.platform.billing.interfaces.rest.transform.BillingAccountResourceFromEntityAssembler;
 import com.nistra.demy.platform.billing.interfaces.rest.transform.CreateBillingAccountCommandFromResourceAssembler;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -32,6 +31,16 @@ public class BillingAccountsController {
     public ResponseEntity<BillingAccountResource> createBillingAccount(@RequestBody CreateBillingAccountResource resource) {
         var createBillingAccountCommand = CreateBillingAccountCommandFromResourceAssembler.toCommandFromResource(resource);
         var billingAccount = billingAccountCommandService.handle(createBillingAccountCommand);
+        if (billingAccount.isEmpty()) return ResponseEntity.badRequest().build();
+        var billingAccountEntity = billingAccount.get();
+        var billingAccountResource = BillingAccountResourceFromEntityAssembler.toResourceFromEntity(billingAccountEntity);
+        return new ResponseEntity<>(billingAccountResource, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{billingAccountId}/invoices")
+    public ResponseEntity<BillingAccountResource> assignInvoice(@PathVariable Long billingAccountId, @RequestBody AssignInvoiceResource resource) {
+        var assignInvoiceCommand = AssignInvoiceCommandFromResourceAssembler.toCommandFromResource(billingAccountId, resource);
+        var billingAccount = billingAccountCommandService.handle(assignInvoiceCommand);
         if (billingAccount.isEmpty()) return ResponseEntity.badRequest().build();
         var billingAccountEntity = billingAccount.get();
         var billingAccountResource = BillingAccountResourceFromEntityAssembler.toResourceFromEntity(billingAccountEntity);
