a0dcb5abdb754651e610ffec98e88fdb3da9f064
Salim Ramirez <salimramirezm@gmail.com>
2025-10-27 19:35:01 -0500

    feat(billing): add BillingAccountQueryService and GetAllInvoicesByBillingAccountIdQuery for invoice retrieval

---
+++ src/main/java/com/nistra/demy/platform/billing/domain/services/BillingAccountQueryService.java
@@ -0,0 +1,12 @@
+package com.nistra.demy.platform.billing.domain.services;
+
+import com.nistra.demy.platform.billing.domain.model.entities.Invoice;
+import com.nistra.demy.platform.billing.domain.model.queries.GetAllInvoicesByBillingAccountIdQuery;
+import com.nistra.demy.platform.billing.domain.model.queries.GetAllInvoicesByStudentIdQuery;
+
+import java.util.List;
+
+public interface BillingAccountQueryService {
+    List<Invoice> handle(GetAllInvoicesByBillingAccountIdQuery query);
+
+}
