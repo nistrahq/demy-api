5d193577597734db9880058bf8a476e3e676411d
Salim Ramirez <salimramirezm@gmail.com>
2025-11-04 12:48:59 -0500

    feat(accounting-finance): add custom exceptions for invalid transaction categories, methods, and types

---
+++ src/main/java/com/nistra/demy/platform/accountingfinance/domain/exceptions/InvalidTransactionMethodException.java
@@ -0,0 +1,9 @@
+package com.nistra.demy.platform.accountingfinance.domain.exceptions;
+
+import com.nistra.demy.platform.shared.domain.exceptions.DomainException;
+
+public class InvalidTransactionMethodException extends DomainException {
+    public InvalidTransactionMethodException(String method) {
+        super("Invalid transaction method %s".formatted(method));
+    }
+}
