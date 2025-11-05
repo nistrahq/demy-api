141af4b99b5684c6a9da999aaaa8bc967cac0821
Salim Ramirez <salimramirezm@gmail.com>
2025-11-04 20:09:03 -0500

    feat(accounting-finance): enable dynamic transaction filtering by extending JpaSpecificationExecutor in TransactionRepository

--- src/main/java/com/nistra/demy/platform/accountingfinance/infrastructure/persistence/jpa/repositories/TransactionRepository.java
+++ src/main/java/com/nistra/demy/platform/accountingfinance/infrastructure/persistence/jpa/repositories/TransactionRepository.java
@@ -2,8 +2,9 @@ package com.nistra.demy.platform.accountingfinance.infrastructure.persistence.jp
 
 import com.nistra.demy.platform.accountingfinance.domain.model.aggregates.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long>, JpaSpecificationExecutor<Transaction> {
 }
