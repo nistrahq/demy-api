004d1a4d72ce00257ec3bd3defe8b9e0e240284d
Salim Ramirez <salimramirezm@gmail.com>
2025-11-04 20:09:29 -0500

    feat(accounting-finance): add method to convert a collection of Transaction entities to TransactionResource

--- src/main/java/com/nistra/demy/platform/accountingfinance/interfaces/rest/transform/TransactionResourceFromEntityAssembler.java
+++ src/main/java/com/nistra/demy/platform/accountingfinance/interfaces/rest/transform/TransactionResourceFromEntityAssembler.java
@@ -3,6 +3,9 @@ package com.nistra.demy.platform.accountingfinance.interfaces.rest.transform;
 import com.nistra.demy.platform.accountingfinance.domain.model.aggregates.Transaction;
 import com.nistra.demy.platform.accountingfinance.interfaces.rest.resources.TransactionResource;
 
+import java.util.Collection;
+import java.util.List;
+
 public class TransactionResourceFromEntityAssembler {
     public static TransactionResource toResourceFromEntity(Transaction entity) {
         return new TransactionResource(
@@ -15,5 +18,11 @@ public class TransactionResourceFromEntityAssembler {
                 entity.getDescription(),
                 entity.getTransactionDate().toString()
         );
+    }
+
+    public static List<TransactionResource> toResourcesFromEntities(Collection<Transaction> entities) {
+        return entities.stream()
+                .map(TransactionResourceFromEntityAssembler::toResourceFromEntity)
+                .toList();
     }
 }
