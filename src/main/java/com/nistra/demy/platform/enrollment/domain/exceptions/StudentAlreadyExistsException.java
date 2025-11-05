20f11be2d37302f421b69bb7b999fa3eb88599c3
Kyrubi <u20221c486@upc.edu.pe>
2025-11-05 13:21:36 -0500

    feat(enrollment): add custom exceptions for enrollment bounded context

---
+++ src/main/java/com/nistra/demy/platform/enrollment/domain/exceptions/StudentAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.nistra.demy.platform.enrollment.domain.exceptions;
+
+import com.nistra.demy.platform.shared.domain.exceptions.DomainException;
+import com.nistra.demy.platform.shared.domain.model.valueobjects.DniNumber;
+
+public class StudentAlreadyExistsException extends DomainException {
+    public StudentAlreadyExistsException(DniNumber dni) {
+        super("Student with DNI %s already exists in the current academy context.".formatted(dni.dniNumber()), dni);
+    }
+}
