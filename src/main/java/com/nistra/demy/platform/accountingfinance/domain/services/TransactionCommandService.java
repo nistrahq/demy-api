7b71887961b702e2e55ad1f0a121c0683b5d28c6
Salim Ramirez <salimramirezm@gmail.com>
2025-11-09 11:05:15 -0500

    feat(accounting-finance): add handle method for UpdateTransactionCommand in TransactionCommandService

--- src/main/java/com/nistra/demy/platform/accountingfinance/domain/services/TransactionCommandService.java
+++ src/main/java/com/nistra/demy/platform/accountingfinance/domain/services/TransactionCommandService.java
@@ -2,9 +2,12 @@ package com.nistra.demy.platform.accountingfinance.domain.services;
 
 import com.nistra.demy.platform.accountingfinance.domain.model.aggregates.Transaction;
 import com.nistra.demy.platform.accountingfinance.domain.model.commands.RegisterTransactionCommand;
+import com.nistra.demy.platform.accountingfinance.domain.model.commands.UpdateTransactionCommand;
 
 import java.util.Optional;
 
 public interface TransactionCommandService {
     Optional<Transaction> handle(RegisterTransactionCommand command);
+
+    Optional<Transaction> handle(UpdateTransactionCommand command);
 }
