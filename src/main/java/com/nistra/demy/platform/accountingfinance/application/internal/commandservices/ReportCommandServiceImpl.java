e8700d9aa64d73936977e0900f7b73a5b28a198e
Salim Ramirez <salimramirezm@gmail.com>
2025-11-09 16:24:16 -0500

    feat(accounting-finance): implement ReportCommandServiceImpl for generating transaction PDF reports

---
+++ src/main/java/com/nistra/demy/platform/accountingfinance/application/internal/commandservices/ReportCommandServiceImpl.java
@@ -0,0 +1,36 @@
+package com.nistra.demy.platform.accountingfinance.application.internal.commandservices;
+
+import com.nistra.demy.platform.accountingfinance.application.internal.outboundservices.pdf.PdfReportingService;
+import com.nistra.demy.platform.accountingfinance.domain.model.queries.GetAllTransactionsQuery;
+import com.nistra.demy.platform.accountingfinance.domain.model.valueobjects.TransactionCategory;
+import com.nistra.demy.platform.accountingfinance.domain.model.valueobjects.TransactionMethod;
+import com.nistra.demy.platform.accountingfinance.domain.model.valueobjects.TransactionType;
+import com.nistra.demy.platform.accountingfinance.domain.services.ReportCommandService;
+import com.nistra.demy.platform.accountingfinance.domain.services.TransactionQueryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportCommandServiceImpl implements ReportCommandService {
+
+    private final TransactionQueryService transactionQueryService;
+    private final PdfReportingService pdfReportingService;
+
+    public ReportCommandServiceImpl(
+            TransactionQueryService transactionQueryService,
+            PdfReportingService pdfReportingService
+    ) {
+        this.transactionQueryService = transactionQueryService;
+        this.pdfReportingService = pdfReportingService;
+    }
+
+    @Override
+    public byte[] generateTransactionsPdfReport(
+            TransactionCategory category,
+            TransactionMethod method,
+            TransactionType type
+    ) {
+        var query = new GetAllTransactionsQuery(category, method, type);
+        var transactions = transactionQueryService.handle(query);
+        return pdfReportingService.generateTransactionsPdfReport(transactions);
+    }
+}
