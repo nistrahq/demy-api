bcffd4f2c3ecf0e369b3c531e4e9340ea000f7ac
Salim Ramirez <salimramirezm@gmail.com>
2025-11-11 15:41:10 -0500

    feat(enrollment): add query for retrieving student ID by DNI number

--- src/main/java/com/nistra/demy/platform/enrollment/domain/services/StudentQueryService.java
+++ src/main/java/com/nistra/demy/platform/enrollment/domain/services/StudentQueryService.java
@@ -2,7 +2,9 @@ package com.nistra.demy.platform.enrollment.domain.services;
 
 import com.nistra.demy.platform.enrollment.domain.model.aggregates.Student;
 import com.nistra.demy.platform.enrollment.domain.model.queries.*;
+import com.nistra.demy.platform.shared.domain.model.valueobjects.DniNumber;
 import com.nistra.demy.platform.shared.domain.model.valueobjects.EmailAddress;
+import com.nistra.demy.platform.shared.domain.model.valueobjects.StudentId;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +14,5 @@ public interface StudentQueryService {
     List<Student> handle(GetAllStudentsQuery query);
     Optional<Student> handle(GetStudentByDniQuery query);
     Optional<EmailAddress> handle(GetStudentEmailAddressByUserIdQuery query);
+    Optional<StudentId> handle(GetStudentIdByDniQuery query);
 }
